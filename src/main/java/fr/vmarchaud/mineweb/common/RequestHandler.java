cc2d24fbf3d7f31b19ba1520d500c46913f959e4
ɧσℓσ <paulinsegura79@gmail.com>
2021-06-22 19:04:39 +0200

    disable debug mod

--- src/main/java/fr/vmarchaud/mineweb/common/RequestHandler.java
+++ src/main/java/fr/vmarchaud/mineweb/common/RequestHandler.java
@@ -64,7 +64,7 @@ public class RequestHandler {
 		// register handshake endpoint
 		api.getHTTPRouter().post("/handshake", (request) -> this.handleHandshake(request.getRequest()));
 		api.getHTTPRouter().post("/ask", (request) -> this.handle(request.getRequest()));
-		debug = /*System.getenv("DEBUG") != null && System.getenv("DEBUG").equals("true")*/true;
+		debug = System.getenv("DEBUG") != null && System.getenv("DEBUG").equals("true");
 	}
 	
 	/**
@@ -153,11 +153,10 @@ public class RequestHandler {
 			if (debug) e.printStackTrace();
 			return HttpResponseBuilder.status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		for(Command command : requests) {
 			IMethod 	method = api.getMethods().get(command.getName());
 			Object[] 	inputs = command.getArgs();
-			
 			// if we didnt found the method just continue
 			if (method == null) {
 				JsonObject res = new JsonObject();
